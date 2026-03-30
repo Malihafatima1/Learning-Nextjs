15166b37650b639de74f2331a72b9bad9fa7248c
Maliha Fatima <maliha@localhost.localdomain>
2026-03-30 16:17:30 +0530

    Updated page.js

--- next-dashboard-app/app/dashboard/page.js
+++ next-dashboard-app/app/dashboard/page.js
@@ -1,3 +1,5 @@
-export default function DashboardPage() {
-    return <h2>Dashboard Home</h2>;
-  }
+export default async function DashboardPage() {
+  await new Promise((resolve) => setTimeout(resolve, 3000)); // delay
+
+  return <h2>Dashboard Home</h2>;
+}
--- next-dashboard-app/app/layout.js
+++ next-dashboard-app/app/layout.js
@@ -1,26 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-
-export default function RootLayout({
-  children,
-}) {
+export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html>
+      <body>
+        <h1>Main Layout</h1>
+        {children}
+      </body>
     </html>
   );
 }
